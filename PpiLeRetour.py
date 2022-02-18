ced6a1e2885fc1f353915d186367b360de47e74c
Joseph7457 <78625758+Joseph7457@users.noreply.github.com>
2022-02-18 18:04:59 +0100

    Update PpiLeRetour.py

--- PpiLeRetour.py
+++ PpiLeRetour.py
@@ -1,18 +1,37 @@
-import pygame
+Joueur joueur;
+
+void setup()
+{size(1000,1000);
+joueur = new Joueur(200,0);
+}
+
+void draw()
+{
+background(0);
+joueur.run();
+}
 
 
-LARGEUR = 800
-HAUTEUR = 800
+void keyPressed()
+{
+  
+  if (key == ' ')
+  {
+  if (joueur != null && joueur.saut)
+  {joueur.sauter(-3);
+  print("hello");}
+  }
+  
+  if(keyCode == LEFT && (joueur != null) )
+  {
+  joueur.v[0] = -joueur.vitesse;
+  }
+  
+  if(keyCode == RIGHT && (joueur != null) )
+  {
+  joueur.v[0] = joueur.vitesse;
+  }
 
 
-pygame.init()
-ecran = pygame.display.set_mode((LARGEUR, HAUTEUR))
 
-fini = False
-
-while not fini:
-        for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                        fini = True
-        
-        pygame.display.flip() 
+}
